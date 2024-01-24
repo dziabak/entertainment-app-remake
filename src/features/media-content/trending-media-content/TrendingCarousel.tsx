4999bec423f4f92729538d28f3ac67f07f0f036d
dziabak <86873665+dziabak@users.noreply.github.com>
2024-01-24 09:57:11 +0100

    'Carousel' component polish

--- src/features/media-content/trending-media-content/TrendingCarousel.tsx
+++ src/features/media-content/trending-media-content/TrendingCarousel.tsx
@@ -1,3 +1,4 @@
+// EXTERNAL IMPORTS
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 
@@ -8,44 +9,38 @@ const TrendingCarousel = ({
 }) => {
 	const responsive = {
 		desktop: {
-			breakpoint: { max: 3000, min: 1280 },
-			items: 3,
+			breakpoint: { max: 5000, min: 1280 },
+			items: 2,
 			slidesToSlide: 1,
-			partialVisibilityGutter: 60,
+			partialVisibilityGutter: 120,
 		},
 		tablet: {
 			breakpoint: { max: 1280, min: 768 },
-			items: 3,
-			slidesToSlide: 1, // optional, default to 1.
-			partialVisibilityGutter: 40,
+			items: 1,
+			slidesToSlide: 1,
+			partialVisibilityGutter: 200,
 		},
 		mobile: {
 			breakpoint: { max: 768, min: 0 },
-			items: 2,
-			slidesToSlide: 1, // optional, default to 1.
-			partialVisibilityGutter: 20,
+			items: 1,
+			slidesToSlide: 1,
+			partialVisibilityGutter: 120,
 		},
 	};
 
 	return (
 		<Carousel
+			responsive={responsive}
 			partialVisible={true}
 			swipeable={true}
 			draggable={false}
-			showDots={false}
-			responsive={responsive}
-			centerMode={false}
-			ssr={false} // means to render carousel on server-side.
 			infinite={true}
 			autoPlay={true}
-			autoPlaySpeed={2000}
-			// keyBoardControl={true}
-			// customTransition="all .5"
-			transitionDuration={500}
+			autoPlaySpeed={5000}
+			transitionDuration={1000}
 			containerClass="carousel-container"
-			// containerClass=""
 			removeArrowOnDeviceType={["tablet", "mobile"]}
-			itemClass="pr-4">
+			itemClass="pr-6 h-[140px] md:h-[230px]">
 			{children}
 		</Carousel>
 	);

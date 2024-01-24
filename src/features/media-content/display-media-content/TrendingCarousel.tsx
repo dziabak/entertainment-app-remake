cc77965d65b6a8a9fd0eb86200d0a5f90da8b834
dziabak <86873665+dziabak@users.noreply.github.com>
2024-01-24 15:17:39 +0100

    'TrendingCarousel' component polish for responsive layout

--- src/features/media-content/display-media-content/TrendingCarousel.tsx
+++ src/features/media-content/display-media-content/TrendingCarousel.tsx
@@ -15,13 +15,13 @@ const TrendingCarousel = ({
 			partialVisibilityGutter: 120,
 		},
 		tablet: {
-			breakpoint: { max: 1280, min: 768 },
-			items: 1,
+			breakpoint: { max: 1280, min: 760 },
+			items: 2,
 			slidesToSlide: 1,
-			partialVisibilityGutter: 200,
+			partialVisibilityGutter: 60,
 		},
 		mobile: {
-			breakpoint: { max: 768, min: 0 },
+			breakpoint: { max: 760, min: 0 },
 			items: 1,
 			slidesToSlide: 1,
 			partialVisibilityGutter: 120,
@@ -38,10 +38,7 @@ const TrendingCarousel = ({
 			autoPlay={true}
 			autoPlaySpeed={5000}
 			transitionDuration={1000}
-			// containerClass="carousel-container"
-			// containerClass=""
 			removeArrowOnDeviceType={["tablet", "mobile"]}
-			// itemClass="pr-6 h-[140px] md:h-[230px]">
 			itemClass="pr-6 h-full">
 			{content}
 		</Carousel>

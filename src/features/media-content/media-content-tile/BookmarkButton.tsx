b9dcaf6bacf7d8ba03d8a3af64be05365c6a1fb8
dziabak <86873665+dziabak@users.noreply.github.com>
2024-01-27 00:07:19 +0100

    Added hover states for 'BookmarkButton' Tweaked arrows for trending carousel (changes made in package .css file)

--- src/features/media-content/media-content-tile/BookmarkButton.tsx
+++ src/features/media-content/media-content-tile/BookmarkButton.tsx
@@ -6,15 +6,15 @@ type BookmarkButtonProps = {
 const BookmarkButton = ({ isBookmarked, onClick }: BookmarkButtonProps) => {
 	return (
 		<div className="absolute right-4 top-4">
-			<div className=""></div>
 			<button
 				onClick={onClick}
-				className="flex items-center justify-center w-10 h-10 p-3 rounded-full bg-black/40">
+				className="flex items-center justify-center w-10 h-10 p-3 rounded-full bg-black/40 group hover:bg-white/100">
 				{isBookmarked ? (
 					<svg width="12" height="14" xmlns="http://www.w3.org/2000/svg">
 						<path
 							d="M10.61 0c.14 0 .273.028.4.083a1.03 1.03 0 0 1 .657.953v11.928a1.03 1.03 0 0 1-.656.953c-.116.05-.25.074-.402.074-.291 0-.543-.099-.756-.296L5.833 9.77l-4.02 3.924c-.218.203-.47.305-.756.305a.995.995 0 0 1-.4-.083A1.03 1.03 0 0 1 0 12.964V1.036A1.03 1.03 0 0 1 .656.083.995.995 0 0 1 1.057 0h9.552Z"
 							fill="#FFF"
+							className="group-hover:fill-c-black"
 						/>
 					</svg>
 				) : (
@@ -24,6 +24,7 @@ const BookmarkButton = ({ isBookmarked, onClick }: BookmarkButtonProps) => {
 							stroke="#FFF"
 							strokeWidth="1.5"
 							fill="none"
+							className="group-hover:stroke-c-black"
 						/>
 					</svg>
 				)}
